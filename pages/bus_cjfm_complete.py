b19fdc659261e9f2f1cb8b8c7043b28e459141d8
dan.powers <dan.powers@mtahq.org>
2023-03-17 15:46:13 -0400

    add complete versions

---
+++ pages/bus_cjfm_complete.py
@@ -0,0 +1,225 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+import numpy as np
+
+st.header("Bus Customer Journey Time Performance")
+
+# read in data 
+busjourney = pd.read_csv("https://data.ny.gov/api/views/wrt8-4b59/rows.csv?accessType=DOWNLOAD&sorting=true")
+
+# task 1: lets look at the dataset we just read in with the st.dataframe function
+st.write(
+    "This page lets the user interact with customer journey focused metric data to produce their own visualizations and data tables. Each row in the bus customer journey focused metric dataset reflects a bus route, on a type of route, at a time of day, aggregated over a month"
+)
+
+st.markdown("- *Additional Bus Stop Time* is the average time that customers spend waiting at a stop beyond their scheduled wait time")
+
+st.markdown("- *Additional Travel Time* is the average time customers spend onboard a bus beyond their scheduled travel time")
+
+st.markdown("- *Customer Journey Time Performance* is the percentage of customers whose journeys are completed within 5 minutes of the scheduled time.")
+
+st.markdown("**You can use `st.markdown`** to *style* text how you want")
+
+# lets set the month column as datetime so it works with our filters
+busjourney.month = pd.to_datetime(busjourney.month, format='%Y-%m').dt.date
+
+# let's try to make a line chart comparing different routes, and a bar chart, based on different filters, and then to show the data at the end
+# lets start with a date filter
+maxmonth = busjourney.month.max()
+minmonth = busjourney.month.min()
+
+col1, col2 = st.columns(2)
+
+with col1:
+    select_date = st.date_input(
+        "Enter a start and end date",
+        # supplying two dates means that in streamlit, we'll be able to pick a start and end date
+        value=[minmonth, maxmonth],
+        min_value=minmonth,
+        max_value=maxmonth
+    )
+
+
+    # task 2: print the values stored by selectdate
+
+
+    # let's now filter the data to the selected dates - st.dateinput returns a list of length 2 with the start and end date
+    busjourney = busjourney.loc[
+        (busjourney["month"] >= select_date[0]) & (busjourney['month'] <= select_date[1])
+    ]
+
+    # now - lets add a select filter for trip type
+    # note: we could just write out the trip types, but it's better practice to pull directly from the dataset in case the underlying data changes (e.g., there's a new trip type 'Misc', or 'Slightly faster than Local but less fast than Express')
+    # the `unique()` method pulls unique values from a series; the `tolist()` method converts it to a list type  
+    trip_opts = busjourney.trip_type.unique().tolist()
+
+    # let's make sure the options are sorted by using the `sort` method of lists - these modify the object in place, so we don't need to set trip_opts equal to this (and doing so actually breaks things, because the sort method returns None)
+    trip_opts.sort()
+
+    select_trips = st.selectbox(
+        label="Select which trip-types to include",
+        options=trip_opts,
+        # lets make the first option selected by default
+        index=0
+    )
+
+    # lets only compare routes that have the same trip type
+    busjourney = busjourney.loc[busjourney["trip_type"] == select_trips]
+
+    # lets now pull the unique period options
+    period_opts = busjourney["period"].unique().tolist()
+
+    # let's now make a radio button of whether we want peak period or not
+    select_period = st.radio(
+        label="Select a period",
+        options=period_opts,
+        # lets start with the second option
+        index=1
+    )
+
+with col2:
+    # lets now filter the dataset to the selected period with the loc method- so other options we pull will reflect our filter
+    busjourney = busjourney.loc[busjourney['period'] == select_period]
+
+    # now let's add a borough filter. but what if the user wants to compare routes in multiple boroughs? let's use a multiselect
+    boro_opts = busjourney.borough.unique().tolist()
+
+    boro_opts.sort()
+
+    select_boro = st.multiselect(
+        label="Select which boroughs to include",
+        options=boro_opts,
+        # lets make the first borough displayed
+        default=boro_opts[0]
+    )
+
+    # now - let's shrink the dataset tp reflect the selected boroughs
+    # the isin method to series checks if each value in the series is in the list of values supplied to the isin method
+    busjourney = busjourney.loc[busjourney["borough"].isin(select_boro)]
+
+    route_opts = busjourney.route_id.unique().tolist()
+
+    route_opts.sort()
+
+    # finally - lets make a multiselect filter for route
+    select_routes = st.multiselect(
+        label="Select which routes to include",
+        options=route_opts,
+        # lets make 2 routes display
+        default=route_opts[0:2]
+    )
+
+    # and lets filter the bus data to the routes
+    busjourney = busjourney.loc[busjourney["route_id"].isin(select_routes)]
+
+    # we're going to make some charts later - let's pick which metric we want visualized in the chart
+    # accessing the columns attribute of the busjourney dataframe
+    metric_opts = busjourney.columns
+
+    # task 3: let's look at the columns here, and then modify metric_opts to only include the columns we want the user to be able to visualize
+
+    with st.expander("Visualization fields"):
+
+        # this selects numeric columns
+        metric_opts = busjourney.select_dtypes([np.number]).columns
+
+        select_metric = st.selectbox(
+            "Select which metric you want to visualize",
+            metric_opts
+        )
+
+        # lets add a slider filter based on the maximum and minimum values of the selected metric
+        min_slider = busjourney[select_metric].min()
+        max_slider = busjourney[select_metric].max()
+
+        select_slider = st.slider(
+            'Filter the data to the range of the selected metric',
+            value=[min_slider, max_slider],
+            min_value=min_slider,
+            max_value=max_slider
+        )
+
+        busjourney = busjourney.loc[(busjourney[select_metric] >= select_slider[0]) & (busjourney[select_metric] <= select_slider[1])]
+
+        select_bar_title = st.text_input(
+            label="Write a title for the bar chart",
+            value = "Number of customers by borough"
+        )
+
+
+# task 4: these are a lot of selectors. lets organize these selectors in columns with `st.columns` and `st.expander`
+
+# task 5: this is a lot of information straight down. lets organize these charts and the dataframe with `st.tabs()`
+
+tab1, tab2, tab3 = st.tabs(["Data", "Line Chart", "Bar Chart"])
+
+with tab1:
+
+    # let's look at the output dataset
+    st.dataframe(busjourney, use_container_width=True)
+
+with tab2:
+    # let's initialize a chart of this data with altair
+    basechart = alt.Chart(busjourney)
+
+    # lets use the mark_line method to turn this into a line chart, and then the encode method to set our x and y axis
+    bus_lines = (
+        basechart.mark_line(point=True).encode(
+            x = 'month',
+            y = select_metric,
+            color = 'route_id'
+        )
+        .properties(title = f"{select_metric} by month")
+        .interactive()
+    )
+
+    # task 6: something looks off about this - what's the problem? and what should we do to fix it
+    st.altair_chart(bus_lines, use_container_width=True)
+
+with tab3:
+
+    if select_metric == 'number_of_customers':
+        aggregate = 'sum'
+    
+    else:
+        aggregate = 'mean'
+
+    # lets customize the color schemes - https://altair-viz.github.io/user_guide/customization.html#customizing-colors
+    range = ['blue', 'green', '#FD71FF', '#A2A2A2', 'red']
+    domain = ['Bronx', 'Brooklyn', 'Queens', 'Staten Island', 'Manhattan']
+
+    # and lastly, let's create a bar chart this time - comparing selected boroughs
+    bus_bar = (
+        basechart.mark_bar().encode(
+            # alt.x and alt.y let you specify options more directly
+            x = alt.X(field = 'borough'),
+            y = alt.Y(field = select_metric, type='quantitative', aggregate=aggregate),
+            # don't need the color parameter - but because we want a custom color scheme, we'll add it in
+            color = alt.Color(
+                field = 'borough', 
+                scale = alt.Scale(
+                    domain = domain,
+                    range = range
+                )),
+            # we can use the tooltip parameter to make a tooltip
+            tooltip=[
+                alt.Tooltip('borough'), 
+                alt.Tooltip(select_metric, type='quantitative')
+            ]
+        )
+        .properties(title = select_bar_title)
+        .interactive()
+    )
+
+    # task 7: something also looks off about this chart - what's wrong with it? And what do we want to do to fix it
+    st.altair_chart(bus_bar, use_container_width=True)
+
+
+# task 8: it gets a little cluttered looking at all of this together - reorganize the line chart, bar chart, and dataframe so it's more legible
+
+# bonus task: if only one borough is selected, the bar chart isn't that useful. hide the bar chart and tab if only one borough is selected with an `if` statement
+
+# lets let the user download the data they've filtered
+st.download_button("Download data", 
+                   data=busjourney.to_csv(encoding = 'utf-8'), file_name="busjourney.csv")
---
+++ pages/bus_speeds_complete.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import streamlit as st
+import altair as alt
+  
+#TASK #1: Write a Title that says 'Bus Speeds' using streamlit st.title()
+st.title('Visualizing MTA Bus Speed Data')
+
+#Task #2: Write a header for Bus Speeds st.header()
+st.header('Using Python and Streamlit')
+
+#TASK #3: Write description for Bus Speeds data using st.write()
+st.write('Bus data is calculated by divind total mileage over total operating time'
+         ' Distance and time is measured for each  bus route during peak and off-peak hours'
+         )
+
+# Task #4: Read MTA Bus Speeds: Beginning 2020 data upload
+data_url = "https://data.ny.gov/api/views/6ksi-7cxr/rows.csv?accessType=DOWNLOAD&sorting=true"
+
+bus_speed = pd.read_csv(data_url) #transform to pandas dataframe
+
+# Lets look at the types of data in this
+print(bus_speed.dtypes)
+
+#Task 5: Lets look at the dataframe we just read using st.dataframe(bus_speed) #This displays the dataframe using streamlit
+st.dataframe(bus_speed)
+
+# Organize the Streamlit app
+st.header("MTA Bus Speed Data")
+
+st.write("This app displays the average bus speeds for different boroughs in New York City.")
+
+# st.selectbox() creates a dropdown for the user to select one option from
+borough = st.selectbox(
+    label="Select a borough",
+    # Let's make the options of this select box the unique values of boroughs
+    options = bus_speed["borough"].unique().tolist()
+)
+
+# let's look at what type of data the date column is
+print(type(bus_speed.month.iat[0]))
+
+# Convert month column to datetime object, and lets extract the date from that object
+bus_speed['month'] = pd.to_datetime(
+    arg=bus_speed['month'],
+    format='%Y-%m'
+).dt.date
+print(type(bus_speed.month.iat[0]))
+
+# Define date range for the filter
+min_date = bus_speed['month'].min()
+max_date = bus_speed['month'].max()
+
+# Create a date range filter using streamlit
+start_date, end_date = st.date_input(
+    label="Select date range",
+    # giving a value of a list or tuple here lets the user select a date range; if we wanted the user to select an individual date instead, only give one value
+    value=(min_date, max_date),
+    min_value=min_date,
+    max_value=max_date
+)
+
+# Apply filters
+# the .loc method lets us filter
+df_filtered = bus_speed.loc[(bus_speed["month"] >= start_date) & (bus_speed["month"] <= end_date) & (bus_speed["borough"] == borough)]
+
+#Task #6: Calculate average speed by month
+#using groupby to find monthly average total
+df_avg = df_filtered.groupby(["month"], as_index = False).agg(
+        {"total_mileage": "sum", 
+         "total_operating_time": "sum"}
+)
+
+df_avg["average_speed"] = df_avg["total_mileage"] / df_avg["total_operating_time"] #calculating average speed
+
+# pull max - and add 20%
+max_avg_speed = df_avg["average_speed"].max()
+max_avg_speed = max_avg_speed + 0.2 * max_avg_speed
+
+# we can use the rename method of pandas dataframes to make more descriptive columns
+
+#Task #7: Rename columns average_speed column to "Average speed (mph)" and month to "Date"
+df_avg = df_avg.rename(
+    columns = {
+        "average_speed" : "Average speed (mph)",
+        "month" : "Date"
+    }
+)
+
+# Create Altair chart
+chart = alt.Chart(df_avg).mark_line().encode(
+    x=alt.X('Date'), #x-axis
+    y=alt.Y(
+        'Average speed (mph)', 
+        title='Average Bus Speed (mph)', 
+        scale=alt.Scale(zero=True, domain=[0, max_avg_speed])
+    ), #setting up y-axis minimum to be 0
+    tooltip=[
+        alt.Tooltip('Date'), 
+        alt.Tooltip('Average speed (mph)', title='Average Bus Speed (mph)')
+    ]
+).properties(
+    title=f"Average Bus Speed in {borough} from {start_date} to {end_date}"
+)
+
+# Display chart and table
+st.altair_chart(chart, use_container_width=True)
